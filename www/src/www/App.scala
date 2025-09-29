3d2bc40cfa40a333a6494d91c1459d8f3187de21
nguyenyou <tunt081295@gmail.com>
2025-09-29 10:37:47 +0700

    🚢 ship it

--- www/src/www/App.scala
+++ www/src/www/App.scala
@@ -5,19 +5,52 @@ import com.raquo.laminar.api.L.*
 case class App() {
   def apply(): HtmlElement = {
     div(
-      cls("flex gap-4"),
-      Tag(
-        label = "Red",
-        color = "red"
-      )(),
-      Tag(
-        label = "Green",
-        color = "green"
-      )(),
-      Tag(
-        label = "Blue",
-        color = "blue"
-      )()
+      cls("space-y-4"),
+      div(
+        cls("flex gap-4"),
+        Tag(
+          label = "Red",
+          color = "red"
+        )(),
+        Tag(
+          label = "Green",
+          color = "green"
+        )(),
+        Tag(
+          label = "Blue",
+          color = "blue"
+        )()
+      ),
+      div(
+        cls("flex gap-4"),
+        Tag1(
+          label = "Red",
+          color = _.Red
+        )(),
+        Tag1(
+          label = "Green",
+          color = _.Green
+        )(),
+        Tag1(
+          label = "Blue",
+          color = _.Blue
+        )()
+      ),
+      div(
+        cls("flex gap-4"),
+        Tag2(
+          label = "Red",
+          color = Val(_.Red)
+        )(),
+        Tag2(
+          label = "Green",
+          color = Val(_.Green)
+        )(),
+        Tag2(
+          label = "Blue",
+          color = Val(_.Blue)
+        )()
+      )
     )
   }
 }
